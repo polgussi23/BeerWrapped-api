92d67f20086e7a7939d47a9cb85f85b52e299965
Pol Gussinyer <polgussi23@gmail.com>
2026-06-19 19:20:23 +0200

    Changed connection for pool

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -8,8 +8,11 @@ const dbConfig = {
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 };
 
-const connection = await mysql.createConnection(dbConfig);
+const pool = mysql.createPool(dbConfig);
 
-export default connection;
+export default pool;
